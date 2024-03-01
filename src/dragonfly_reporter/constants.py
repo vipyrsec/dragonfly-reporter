d2ad499f56e1dada3bc0792bfc17804ee2110b00
jonathan-d-zhang <69145546+jonathan-d-zhang@users.noreply.github.com>
2024-03-01 09:20:29 -0500

    feat: Add pydantic model for reading config

---
+++ src/dragonfly_reporter/constants.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class EnvConfig(BaseSettings):
+    """Our default configuration for models that should load from .env files."""
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="__",
+        extra="ignore",
+    )
+
+
+class Reporter(EnvConfig):
+    observation_api_token: str
+
+
+reporter_settings = Reporter()  # pyright: ignore
