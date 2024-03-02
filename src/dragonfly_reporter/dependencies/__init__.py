0d1216b23d2b8e78c72fe9799f398453ea35e1bb
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-03-02 22:51:47 +0000

    Setup dependency for MS Graph
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/dragonfly_reporter/dependencies/__init__.py
@@ -0,0 +1,7 @@
+"""Dependency definitions."""
+
+from .microsoft_graph_dependencies import build_graph_client
+
+__all__ = [
+    "build_graph_client",
+]
---
+++ src/dragonfly_reporter/dependencies/microsoft_graph_dependencies.py
@@ -0,0 +1,16 @@
+"""Microsoft Graph integration."""
+
+from azure.identity.aio import ClientSecretCredential
+from msgraph import GraphServiceClient
+
+from dragonfly_reporter.constants import Microsoft
+
+
+def build_graph_client() -> GraphServiceClient:
+    """Build authenticated GraphClient."""
+    credential = ClientSecretCredential(
+        tenant_id=Microsoft.tenant_id,
+        client_id=Microsoft.client_id,
+        client_secret=Microsoft.client_secret,
+    )
+    return GraphServiceClient(credentials=credential, scopes=Microsoft.scopes)
