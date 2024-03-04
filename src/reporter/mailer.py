11f2f8270299e34b7ba57caab3b1224369fd9934
Robin5605 <robinjefferson123@gmail.com>
2024-03-04 15:49:08 -0600

    Add method for building email body
    Add a method in `mailer.py` that will build a report email body

--- src/reporter/mailer.py
+++ src/reporter/mailer.py
@@ -1,6 +1,7 @@
 """Sending emails."""
 
 from logging import getLogger
+from typing import Optional
 
 from msgraph import GraphServiceClient
 from msgraph.generated.models.attachment import Attachment
@@ -12,10 +13,32 @@ from msgraph.generated.models.recipient import Recipient
 from msgraph.generated.users.item.send_mail.send_mail_post_request_body import (
     SendMailPostRequestBody,
 )
-
+from textwrap import dedent
 from reporter.constants import Mail
+from reporter.utils.pypi import file_path_from_inspector_url
 
 logger = getLogger(__name__)
+
+def build_report_email_content(
+    *,
+    name: str,
+    version: str,
+    inspector_url: str,
+    rules_matched: list[str],
+    additional_information: Optional[str],
+) -> str:
+    content = f"""
+        PyPI Malicious Package Report
+        -
+        Package Name: {name}
+        Version: {version}
+        File path: {file_path_from_inspector_url(inspector_url)}
+        Inspector URL: {inspector_url}
+        Additional Information: {additional_information or "No user description provided"}
+        Yara rules matched: {", ".join(rules_matched) or "No rules matched"}
+    """
+
+    return dedent(content)
 
 
 async def send_mail(
